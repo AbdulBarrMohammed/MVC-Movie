c37cfcd6d0441fcea076afda2af4508298c3cf58
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-06-04 12:49:53 -0400

    delete function works

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7679f79479d160b6db30e029f6b186eadc8bb97a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39ec5abe423ec36874e0a0a03ca997999800f7ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
