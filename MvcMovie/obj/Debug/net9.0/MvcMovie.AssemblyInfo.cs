7679f79479d160b6db30e029f6b186eadc8bb97a
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-06-04 12:06:44 -0400

    movie genre model

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eea8b2771a6bf39b3a8154a061fd948f5f50811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12889e8036898dc40b125a106f29148e94163356")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
